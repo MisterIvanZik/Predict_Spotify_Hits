d162210d5bc02574fab4615c3785e0924e193876
Ivan Zazic <zazicivan@gmail.com>
2025-02-05 11:34:49 +0100

    feat: delete environment option, only was here for testing our application

--- spotify_dashboard/run.sh
+++ spotify_dashboard/run.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
 # Installation des dépendances Python si nécessaire
-if [ ! -d "venv" ]; then
-    echo "🔧 Création de l'environnement virtuel..."
-    python3 -m venv venv
-    source venv/bin/activate
-    echo "📦 Installation des dépendances..."
+echo "📦 Installation des dépendances..."
     pip install -r requirements.txt
-else
-    source venv/bin/activate
-fi
 
 # Lancement de l'application Flask
 echo "🚀 Démarrage du serveur..."
